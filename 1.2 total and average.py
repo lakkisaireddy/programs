6fe9eef5ae8a0692a2829a45ab212f7884f332b2
lakkisaireddy <84392522+lakkisaireddy@users.noreply.github.com>
2021-05-29 14:18:43 +0530

    Add files via upload

---
+++ 1.2 total and average.py
@@ -0,0 +1,9 @@
+x=float(input("Enter 1st number:"))
+y=float(input("Enter 2nd number:"))
+z=float(input("Enter 3rd number:"))
+sum=x+y+z
+print("The sum of three number is:",sum) 
+avr=sum/3
+print("Average is:",avr)
+print("Maximum number is:",max(x,y,z))
+print("Minimum number is:",min(x,y,z))
